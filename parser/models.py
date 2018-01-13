2631d48ee5eaf34bc9d0358295c560fc67abe8b0
unknown <andyworms@gmail.com>
2018-01-13 23:05:03 +0200

    justice centered

---
+++ parser/models.py
@@ -0,0 +1,160 @@
+CaseFields = [
+        "Confidential",
+        "caseId",
+        "issueIdHebrew",
+        "issueId",
+        "caseName",
+        "petitioner1",
+        "petitioner2",
+        "petitioner3",
+        "respondent1",
+        "respondent2",
+        "respondent3",
+        "lawyerP1",
+        "lawyerP2",
+        "lawyerP3",
+        "lawyerR1",
+        "lawyerR2",
+        "lawyerR3",
+        "NPetitioners",
+        "NRespondents",
+        "parliament",
+        "courtCaseSource",
+        "numCaseSource",
+        "dateCaseSource",
+        "dateOpen",
+        "dateFinalDecision",
+        "numHearings",
+        "dateFArgument",
+        "dateLArgument",
+        "LIssue",
+        "LIssueArea",
+        "justice1",
+        "justice2",
+        "justice3",
+        "justice4",
+        "justice5",
+        "justice6",
+        "justice7",
+        "justice8",
+        "justice8",
+        "justice10",
+        "justice11",
+        "justice12",
+        "justice13",
+        "justice14",
+        "justice15",
+        "Njudges",
+        "Npages"
+]
+
+JudgesFields = [
+        "Title",
+        "Name",
+        "Retired",
+        "Gender",
+        "caseId",
+        "issueIdHebrew",
+        "issueId",
+        "caseName",
+        "petitioner1",
+        "petitioner2",
+        "petitioner3",
+        "respondent1",
+        "respondent2",
+        "respondent3",
+        "lawyerP1",
+        "lawyerP2",
+        "lawyerP3",
+        "lawyerR1",
+        "lawyerR2",
+        "lawyerR3",
+        "NPetitioners",
+        "NRespondents",
+        "parliament",
+        "courtCaseSource",
+        "numCaseSource",
+        "dateCaseSource",
+        "dateOpen",
+        "dateFinalDecision",
+        "numHearings",
+        "dateFArgument",
+        "dateLArgument",
+        "LIssue",
+        "LIssueArea",
+        "justice1",
+        "justice2",
+        "justice3",
+        "justice4",
+        "justice5",
+        "justice6",
+        "justice7",
+        "justice8",
+        "justice8",
+        "justice10",
+        "justice11",
+        "justice12",
+        "justice13",
+        "justice14",
+        "justice15",
+        "Njudges",
+        "Npages"
+]
+
+class Case:
+        Confidential = ""
+        caseId = ""
+        issueIdHebrew = ""
+        issueId = ""
+        caseName = ""
+        petitioner1 = ""
+        petitioner2 = ""
+        petitioner3 = ""
+        respondent1 = ""
+        respondent2 = ""
+        respondent3 = ""
+        lawyerP1 = ""
+        lawyerP2 = ""
+        lawyerP3 = ""
+        lawyerR1 = ""
+        lawyerR2 = ""
+        lawyerR3 = ""
+        NPetitioners = ""
+        NRespondents = ""
+        parliament = ""
+        courtCaseSource = ""
+        numCaseSource = ""
+        dateCaseSource = ""
+        dateOpen = ""
+        dateFinalDecision = ""
+        numHearings = ""
+        dateFArgument = ""
+        dateLArgument = ""
+        LIssue = ""
+        LIssueArea = ""
+        Npages = ""
+        justice1 = ""
+        justice2 = ""
+        justice3 = ""
+        justice4 = ""
+        justice5 = ""
+        justice6 = ""
+        justice7 = ""
+        justice8 = ""
+        justice9 = ""
+        justice10 = ""
+        justice11 = ""
+        justice12 = ""
+        justice13 = ""
+        justice14 = ""
+        justice15 = ""
+        justice14 = ""
+        Njudges = ""
+
+
+class Justice:
+    Title =""
+    Name = ""
+    Retired = ""
+    Gender = ""
+
